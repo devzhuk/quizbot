4b3301f40d921f175ea5e90b131af7acf7da04a5
Anton Zhukov <devzhukov@gmail.com>
2024-01-20 15:51:27 +0300

    initial commit

--- src/main/kotlin/ru/zhukov/quizbot/QuizBotApplication.kt
+++ src/main/kotlin/ru/zhukov/quizbot/QuizBotApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class QuizbotApplication
+class QuizBotApplication
 
 fun main(args: Array<String>) {
-	runApplication<QuizbotApplication>(*args)
+	runApplication<QuizBotApplication>(*args)
 }
